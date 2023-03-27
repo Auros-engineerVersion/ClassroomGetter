65df092c392dc49aaac984e50c39729ab5a02490
Auros-NotePC <92504306+Auros-engineerVersion@users.noreply.github.com>
2023-03-27 12:20:03 +0900

    change the controls

--- src/controls.py
+++ src/controls.py
@@ -3,8 +3,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup as bs
 from .factory import create_driver
-import time
 from urllib.parse import urljoin
+import time
 
 class Controls:
     def __init__(self, profile_path: str, profile_name: str) -> None:
@@ -24,24 +24,18 @@ class Controls:
         self._soupObj = bs(self._current_html, 'html.parser')
     
 # public
-    def move(self, url: str, argBy: By, argTarget: str):
+    def move(self, url: str):
         try:
             self._driver.get(url)
-            self._wait(EC.visibility_of_all_elements_located((argBy, argTarget)))
+            time.sleep(2)
             self._set_current_data()
-                            
-        except BaseException as e:
-            print('\033[31m')
-            print(e)
-            print('\033[0m')
-            
         finally:
             return self # Linqライクにするため
         
     # classroomに入った時点に表示される科目のurlを取得する
     def lesson_links(self, class_name: str):
         base_url = self._soupObj.find('base')['href']
-        lessons = self._soupObj.find_all('a', class_='onkcGd ZmqAt Vx8Sxd') #aタグを検索
+        lessons = self._soupObj.find_all('a', class_=class_name) #aタグを検索
         links = [urljoin(base_url, value['href']) for value in lessons]
         
         return links
--- test/controls_test.py
+++ test/controls_test.py
@@ -11,21 +11,27 @@ from selenium.webdriver.support import expected_conditions as EC
 class TestControls(unittest.TestCase):
     def setUp(self) -> None:
         self.controls = Controls(cfg.PROFILE_PATH, cfg.PROFILE_NAME)
-        
+        return super().setUp()
+    
+    def tearDown(self) -> None:
+        del self.controls
+        return super().tearDown()
+
     def test_can_move(self):
-        self.controls.move(cfg.TARGET_URL, By.CLASS_NAME, cfg.LESSON_CLASS_NAME)
+        self.controls.move(cfg.TARGET_URL)
         self.assertEqual(self.controls._driver.current_url, cfg.TARGET_URL)
         
     def test_links(self):
-        self.controls.move(cfg.TARGET_URL, By.CLASS_NAME, cfg.LESSON_CLASS_NAME)
+        self.controls.move(cfg.TARGET_URL)
         self.controls.lesson_links(cfg.LESSON_CLASS_NAME)
         self.assertTrue(True)
                 
 try:
     if __name__ == '__main__':
         unittest.main()
-    
-except BaseException as e:
+        
+except Exception as e:
     print('\033[31m')
+    print(type(e))
     print(e)
     print('\033[0m')
