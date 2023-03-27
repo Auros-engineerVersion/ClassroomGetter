72decbbb5ed3ea4615812bd6af7c738b880c7208
Auros-NotePC <92504306+Auros-engineerVersion@users.noreply.github.com>
2023-03-27 12:25:21 +0900

    main exception change

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ from src.controls import Controls
 try:
     controls = Controls(cfg.PROFILE_PATH, cfg.PROFILE_NAME)
     controls.move(cfg.TARGET_URL)
-    
-except BaseException as e:
+except Exception as e:
     print('\033[31m' + e.args + '\033[0m')
-    
 finally:
     cfg.back_origin_enviroment()
     del controls
