52e4aa6ff53d295f6d35087d743b36c830c0c032
Auros-NotePC <92504306+Auros-engineerVersion@users.noreply.github.com>
2023-03-31 21:52:15 +0900

    change the controls.py

--- src/controls.py
+++ src/controls.py
@@ -1,36 +1,47 @@
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import InvalidArgumentException
+from . import custum_condition as MyEC
 from .factory import create_driver
-import re
 
 class Controls:
     def __init__(self, profile_path: str, profile_name: str) -> None:
-        self._driver = create_driver(profile_path, profile_name)
-        self._wait = WebDriverWait(self._driver, 30)
+        self.__driver = create_driver(profile_path, profile_name)
+        self.__wait = WebDriverWait(self.__driver, 30)
         pass
     
     def __del__(self):
-        del self._wait
-        self._driver.quit()
-        del self._driver
+        del self.__wait
+        self.__driver.quit()
+        del self.__driver
         pass
     
-# public
-    def move(self, url: str):
+    @staticmethod
+    def move(driver: webdriver, url: str):
         try:
-            self._driver.get(url)
-        finally:
-            return self # Linqライクにするため
+            driver.get(url)
+        except InvalidArgumentException as e:
+            raise e
         
-    def hrefs(self, class_name: str):
-        elems = self._wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, 'a')))
+    @staticmethod
+    def hrefs(wait: WebDriverWait, *conditions: str):
+        elems = wait.until(MyEC.document_state_is((By.TAG_NAME, 'a'), 'complete'))
         links = set()
+        
         for elem in elems:
-            link = elem.get_attribute("href")
-            #文字列を含むなら
-            if ('https://classroom.google.com/u/0/c/' in link):
-                if not ('/sp/' in link):
+            link = elem.get_attribute('href')
+            #全ての条件に合致するなら
+            for condition in conditions:
+                if (condition in link):
                     links.add(link)
-                
+                    
         return links
+                
+    @classmethod
+    def move(cls, url: str):
+        Controls.move(cls.__driver, url)
+        
+    @classmethod
+    def hrefs(cls, *conditions: str):
+        return Controls.hrefs(cls.__wait, conditions)
