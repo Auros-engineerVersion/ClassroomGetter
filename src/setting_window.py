a5e2b894fe1d4021c1b3f6ef9676e2b8ce2d5ab3
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-04-14 17:45:30 +0900

    change the setting_window.py

--- src/setting_window.py
+++ src/setting_window.py
@@ -3,11 +3,13 @@ from tkinter import ttk
 
 class Window(tk.Frame):
     @staticmethod
-    def InputForm(func = None, *args, master: tk.Misc = None):
-        def __function_is_not_null(func = None, *args):
-            if (func != None):
-                return func(*args)
-                
+    def __GetData(frame: tk.Frame):
+        for child in frame.winfo_children():
+            if (type(child) == tk.Entry):
+                return child.get()
+    
+    @staticmethod
+    def InputForm(master: tk.Misc = None) -> tuple:
         if (master == None):
             master = tk.Tk()
         
@@ -15,11 +17,11 @@ class Window(tk.Frame):
         master.resizable(0, 0) #windowのサイズ変更を許可しない
         
         padx_size = (5, 10)
-        width      = 30
+        width = 30
         
         email = Window.InputBox(master, width, padx_size, 'email')
         password = Window.InputBox(master, width, padx_size, 'password')
-        complete_button = tk.Button(master, text='完了', command=lambda:__function_is_not_null(func, *args))
+        complete_button = tk.Button(master, text='完了', command=master.quit)
 
         # ウィジェットの配置
         email.pack(side=tk.TOP, anchor=tk.W, fill=tk.X)
@@ -27,8 +29,12 @@ class Window(tk.Frame):
         complete_button.pack(side=tk.BOTTOM, anchor=tk.E, padx=5, pady=5)
         
         master.mainloop()
-        
-        complete_button.
+                
+        #入力された値を取得して返す
+        email_info = Window.__GetData(email)
+        password_info = Window.__GetData(password)
+        master.destroy() #値を取得できたらフォームを解放する
+        return (email_info, password_info)
             
     @staticmethod
     def InputBox(master: tk.Misc, width: int, padx: int, title = '') -> tk.Frame:
@@ -38,8 +44,9 @@ class Window(tk.Frame):
         
         label.pack(side=tk.LEFT, anchor=tk.W, ipadx=1, padx=padx)
         t_box.pack(side=tk.RIGHT, anchor=tk.E, ipadx= 1, padx=padx[:-1]) #tupleを反転させる
-        
+                
         return frame
-    
+
 if __name__ == '__main__':
-    Window.InputForm()
+    for value in Window.InputForm():
+        print(value)
