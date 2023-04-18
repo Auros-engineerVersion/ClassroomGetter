7e0a4e9bb86ad25b2620351c7f5ce6d00e4ad791
Auros-NotePC <92504306+Auros-engineerVersion@users.noreply.github.com>
2023-04-18 09:39:01 +0900

    change the nodes.py

--- src/browser/nodes.py
+++ src/browser/nodes.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from selenium.webdriver.common.by import By
 from inspect import signature
 
-import my_util
+from src import my_util
 from src.interface.i_node import INode
 from src.browser.browser_controls import BrowserControls as bc
 
@@ -12,12 +12,13 @@ class Node(INode):
     BrowserControl: bc = None
     Nodes: set[INode] = set() #全てのノードの集合
 
-    def __init__(self, key: str, tree_height: int) -> None:
+    def __init__(self, key: str, url: str, tree_height: int) -> None:
         if Node.BrowserControl is None:
             raise TypeError('BrowserControl is None')
         
         self.__edges: list[INode] = []
-        self.key = key 
+        self.key = key
+        self.url = url
         self.tree_height = abs(tree_height) #負の値が入れられないように
         Node.Nodes.add(self)
         pass
@@ -35,13 +36,15 @@ class Node(INode):
             return \
                 self.tree_height == other.tree_height and\
                 self.key         == other.key         and\
+                self.url         == other.url         and\
                 self.edges()     == other.edges()
     
     def __hash__(self) -> int:
-        height_hash = hash(self.tree_height)
         key_hash    = hash(self.key)
+        url_hash    = hash(self.url)
+        height_hash = hash(self.tree_height)
         
-        return hash(height_hash + key_hash)
+        return hash((key_hash, url_hash, height_hash))
     
     def __del__(self) -> None:
         if (self in Node.Nodes):
@@ -63,35 +66,52 @@ class Node(INode):
             self.__edges.append(add_value)
             Node.Nodes.add(add_value)
     
-    def next_links(self) -> list[str]:
+    def next_links(self) -> list[tuple[str, str]]: #(title, url)
         c = self.BrowserControl
 
         #ホームなら
         if (self.tree_height == 0):
             c.move(self.key)
-            locator = (By.XPATH, "//a[@class='onkcGd ZmqAt Vx8Sxd']")
-            pattern = '^.*/c/.{16}$'
-            return c.hrefs()(locator, pattern)
+            link_locator = (By.XPATH, "//a[@class='onkcGd ZmqAt Vx8Sxd']")
+            link_pattern = '^.*/c/.{16}$'
+            links = c.elements(link_locator, link_pattern)(lambda elem: elem.get_attribute('href'))
+            
+            title_locator = (By.XPATH, "//div[@class='YVvGBb z3vRcc-ZoZQ1']")
+            title_pattern = ''
+            titles = list(
+                map(
+                    my_util.text_filter,
+                    c.elements(title_locator, title_pattern)(lambda elem: elem.text)
+                ))
+            
+            return my_util.convert_to_tuple(titles, links)
+                
         #授業のタブなら
         #https://classroom.google.com/c/NjAyMTUwMTQyNzk1
         elif (self.tree_height == 1):
-            return [my_util.to_all_tab_link(self.key)]
+            return [(self.key + '_授業タブ', my_util.to_all_tab_link(self.url))]
         
         #「全てのトピック」なら
-        elif ('/t/all' in self.key):
-            c.move(self.key)
-            locator = (By.XPATH, "//a[@class='VkhHKd e7EEH ']")
-            pattern = '^.*/file/d/.*$'
-            return c.click_all_sections(c.hrefs, (locator, pattern))
+        elif ('/t/all' in self.url):
+            c.move(self.url)
+            file_locator = (By.XPATH, "//a[@class='VkhHKd e7EEH ']")
+            file_pattern = '^.*/file/d/.*$'
+            files = c.click_all_sections(c.elements(file_locator, file_pattern), lambda elem: elem.get_attribute('href'))
+            
+            name_locator = (By.XPATH, "//div[@class='lIHx8b YVvGBb asQXV ']")
+            name_pattern = ''
+            names = c.elements(name_locator, name_pattern)(lambda elem: elem.text)
+            
+            return my_util.convert_to_tuple(names, files)
         
         #ファイルのurlなら
         else:
             return []
             
-    def create_childs(self, *keys: str):
-        for key in keys:
+    def create_childs(self, *args: tuple[str, str]):
+        for tuple in args:
             self.edges(
-                add_value=Node(key, self.tree_height + 1)
+                add_value=Node(*tuple, self.tree_height + 1)
             )
             
         return self
