d2d45eb40215a7fae55c9f52a13f6d8c8eaa90b2
Auros-NotePC <92504306+Auros-engineerVersion@users.noreply.github.com>
2023-04-18 14:20:12 +0900

    change te NodeBox

--- src/gui/custum_widgets.py
+++ src/gui/custum_widgets.py
@@ -1,4 +1,8 @@
+from __future__ import annotations
+import gc
 import tkinter as tk
+
+from src.interface.i_node import INode
 
 class ScrollableFrame(tk.Frame):
     def __init__(self, master: tk.Misc, width: int, height: int, bar_x = True, bar_y = True):
@@ -33,11 +37,52 @@ class InputBox(tk.Frame):
         label.pack(side=tk.LEFT, anchor=tk.W, ipadx=1, padx=padx)
         t_box.pack(side=tk.RIGHT, anchor=tk.E, ipadx= 1, padx=padx[:-1]) #tupleを反転させる
             
-class NodeBox(tk.Button):
-    def __init__(self, master: tk.Misc, node):
-        tk.Button.__init__(self, master) #tree_heightに応じてインデントする
+class NodeBox(tk.Frame):
+    def __init__(self, master: tk.Misc, node: INode, parent: NodeBox = None):
+        tk.Frame.__init__(self, master) #tree_heightに応じてインデントする
+        drop_button = tk.Button(self, command=self.expand, width=self.winfo_height())
         height_label = tk.Label(self, text=str(node.tree_height) + ':')
-        url_label = tk.Label(self, text=str(node.key))
+        key_label = tk.Label(self, text=str(node.key))
+        url_label = tk.Label(self, text=str(node.url))
         
+        drop_button.pack(side=tk.LEFT)
         height_label.pack(side=tk.LEFT)
+        key_label.pack(side=tk.LEFT)
         url_label.pack(side=tk.LEFT)
+                
+        self.__master = master
+        self.__is_expand: bool = False
+        self.__node = node
+        self.__parent_box = parent
+        self.__nextboxes: list[NodeBox] = []
+        
+        self.pack(anchor=tk.W, padx=(node.tree_height*20, 1), after=self.__parent_box)
+                    
+    def dispose(self):
+        stack: list[NodeBox] = [self]
+        
+        while(len(stack) > 0):
+            value = stack.pop()
+            
+            for box in value.__nextboxes:
+                stack.append(box)
+
+            value.destroy()
+        
+    def expand(self):
+        #反転する
+        self.__is_expand = not self.__is_expand
+
+        if (self.__is_expand):
+            self.pack(anchor=tk.W)
+            list(
+                map(
+                    lambda node: self.__nextboxes.append(NodeBox(self.__master, node, self)),
+                    self.__node.edges()
+                )
+            )
+        else:
+            for box in self.__nextboxes:
+                box.dispose()
+                
+            gc.collect()
