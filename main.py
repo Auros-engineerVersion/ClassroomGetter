201f2086ff0d9c62dbf559227da53c8668029b0e
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-04-27 19:10:05 +0900

    change the main.py

--- main.py
+++ main.py
@@ -8,28 +8,14 @@ from src.browser.browser_controls import BrowserControl
 from src.gui.window import Window
 
 try:
-    target_url = 'https://classroom.google.com/' #固定値
-
     #最初の認証
     #gauth = GoogleAuth()
     #gauth.LocalWebserverAuth()
-    cfg = Settings.load(Settings.DefaultSaveFolderPath)
+    cfg = Settings.load(Settings.DEFAULT_SAVEFOLDER_PATH)
     bc = BrowserControl(setting=cfg)
     Node.BrowserControl = bc
-
-    #node_listが何も設定されていないのなら値を取りに行く
-    if not cfg.is_current_data():
-        #プロファイルが指定されているかどうか
-        #されていなければログインして指定する
-        bc.move(target_url)
-        if (target_url not in bc.driver.current_url):
-            bc.login_classroom(cfg)
-
-        root = Node('Classroom', target_url, 0)
-        root.initialize_tree()
-    
-        cfg.node_list = Node.Nodes
-        Settings.save(cfg)
+        
+    Settings.validate_data(cfg, bc)
         
     root = min(cfg.node_list) #tree_height == 0のものをrootとする
     Window.RunWindow(root)
