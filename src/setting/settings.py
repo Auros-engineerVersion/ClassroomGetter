5aaee4e074410057cd6830ea1a6d7b59850b9df9
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-04-27 19:09:27 +0900

    Change the settings.py

--- src/setting/settings.py
+++ src/setting/settings.py
@@ -2,11 +2,14 @@ from __future__ import annotations
 import pickle
 from pathlib import Path
 
+from src.browser.browser_controls import BrowserControl
 from src.data.setting_data import SettingData
+from src.data.nodes import Node
 from src.gui.window import Window
 
 class Settings:
-    DefaultSaveFolderPath = Path('./Save')
+    TARGET_URL = 'https://classroom.google.com/' #固定値
+    DEFAULT_SAVEFOLDER_PATH = Path('./Save')
     
     def encryption(self) -> Settings:
         return self
@@ -44,3 +47,19 @@ class Settings:
     def new_data() -> SettingData:
         user_info = Window.InputForm()
         return SettingData(*user_info)
+    
+    @staticmethod
+    def validate_data(data: SettingData, bc: BrowserControl):
+        #node_listが何も設定されていないのなら値を取りに行く
+        if not data.is_current_data():
+            #プロファイルが指定されているかどうか
+            #されていなければログインして指定する
+            bc.move(Settings.TARGET_URL)
+            if (Settings.TARGET_URL not in bc.driver.current_url):
+                bc.login_classroom(data)
+
+            root = Node('Classroom', Settings.TARGET_URL, 0)
+            root.initialize_tree()
+
+            data.node_list = Node.Nodes
+            Settings.save(data)
