124de7ee63c911974ea55626e5ef5280585fbff0
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-05-05 17:16:21 +0900

    change the test

--- test/settings_test.py
+++ test/settings_test.py
@@ -3,36 +3,46 @@ sys.path.append(os.path.abspath('.'))
 
 import unittest
 from unittest.mock import patch, Mock
+from pathlib import Path
 
-from src.setting.settings import Settings
+from src.setting.settings import Settings, SettingData
 
 class NormaTest(unittest.TestCase):
     def setUp(self) -> None:
-        self.__test_file_path = './test/for_test.pkl'
-        open(self.__test_file_path, 'w')
+        self.__test_folder_path = Path('./test/for_settings_test')
         return super().setUp()
 
     def tearDown(self) -> None:
-        os.remove(self.__test_file_path)
+        os.remove(self.__test_folder_path.joinpath('save.pkl'))
+        os.rmdir(self.__test_folder_path)
         return super().tearDown()
 
-    def test_Save_and_Load(self):
-        target_1 = [*'いろはにほへと']
-        target_2 = [*'ちりぬるお']
-        Settings.Save(self.__test_file_path, *target_1)
-        result = Settings.Load(self.__test_file_path)
+    def test_save_and_load(self):
+        target_1 = SettingData('いろはにほへと', setting_folder_path=self.__test_folder_path)
+        target_2 = SettingData('ちりぬるお', setting_folder_path=self.__test_folder_path)
         
+        Settings.save(target_1)
+        result = Settings.load(self.__test_folder_path)
         self.assertEqual(result, target_1)
         
-        Settings.Save(self.__test_file_path, *target_1)
-        Settings.Save(self.__test_file_path, *target_2)
-        result = Settings.Load(self.__test_file_path)
-        
+        #上書き
+        Settings.save(target_1)
+        Settings.save(target_2)
+        result = Settings.load(self.__test_folder_path)
         self.assertEqual(result, target_2)
     
 class AbNormalTest(unittest.TestCase):
-    def test_constructor(self):
-        self.assertRaises(ValueError, Settings, 'HogeHoge')
-        
+    def setUp(self) -> None:
+        self.__test_folder_path = Path('./test/for_settings_test')
+        self.__test_folder_path.mkdir(parents=True, exist_ok=True)
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        os.rmdir(self.__test_folder_path)
+        return super().tearDown()
+    
+    def test_no_such_file(self):
+        self.assertEqual(Settings.load(self.__test_folder_path), SettingData())
+    
 if __name__ == '__main__':
     unittest.main()
