eb4df48279aa7c7f5578cae95e56562afb94f3d2
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-05-06 12:02:17 +0900

    change the setting_data

--- src/data/setting_data.py
+++ src/data/setting_data.py
@@ -2,10 +2,11 @@ from __future__ import annotations
 from pathlib import Path
 import dataclasses
 
+NO_DATA = 'No Data'
 @dataclasses.dataclass
 class SettingData:
-    user_email: str
-    user_password: str
+    user_email: str     = NO_DATA
+    user_password: str  = NO_DATA
 
     #セーブフォルダの場所
     save_folder_path: Path = Path('./Save')
@@ -22,7 +23,7 @@ class SettingData:
         args = list(
             map(
                 lambda value_1, value_2:
-                    value_1 or value_2,
+                    value_2 or value_1, #右の値を優先する
                 vars(self).values(), vars(other).values()
             )
         )
@@ -31,12 +32,20 @@ class SettingData:
         
     def is_current_data(self):
         return\
-            self.node_list != None              and\
-            len(self.node_list) > 1             and\
-            '@' in self.user_email              and\
-            len(self.user_password) > 0         and\
+            self.is_current_nodes()             and\
+            not self.is_default()               and\
+            self.is_current_user()              and\
             int(self.loading_wait_time) >= 0    and\
             self.setting_folder_path.exists()
+        
+    def is_current_nodes(self):
+        return self.node_list != None and len(self.node_list) > 1
+            
+    def is_current_user(self):
+        return '@' in self.user_email and len(self.user_password) > 0
+            
+    def is_default(self):
+        return self.user_email + self.user_password in NO_DATA
         
     def profile(self) -> Path:
         return Path(self.setting_folder_path).absolute().joinpath('./ProfileData/Profile 1')
