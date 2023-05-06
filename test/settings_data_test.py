6ca180605d732f039b589857df2cd732654c4072
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-05-06 12:38:35 +0900

    change the settings_data_test

--- test/settings_data_test.py
+++ test/settings_data_test.py
@@ -3,6 +3,7 @@ sys.path.append(os.path.abspath('.'))
 
 import unittest
 from unittest.mock import patch, Mock
+from pathlib import Path
 
 from src.data.setting_data import SettingData
 
@@ -17,5 +18,6 @@ class NormalTest(unittest.TestCase):
         sets = set()
         sets.add('Hoge')
         sets.add('bar')
-        current_data = SettingData('hogehoge@gmail.com', 'hogehoge', node_list=sets)
+        directory = Path('.')
+        current_data = SettingData('hogehoge@gmail.com', 'hogehoge', node_list=sets, save_folder_path=directory)
         self.assertTrue(current_data.is_current_data())
