ea4ad86296142c0af43539b6095f9733945a7e06
Auros-NotePC <92504306+Auros-engineerVersion@users.noreply.github.com>
2023-05-16 09:46:58 +0900

    change the test dir

--- test/gui_tests/custum_widgets_test.py
+++
@@ -1,8 +0,0 @@
-import sys, os
-sys.path.append(os.path.abspath('.'))
-
-import unittest
-from unittest.mock import Mock
-
-from src.gui.custum_widgets import *
-
---
+++ test/gui_tests/input_box_test.py
@@ -0,0 +1,29 @@
+import sys, os
+sys.path.append(os.path.abspath('.'))
+
+import unittest
+import tkinter as tk
+
+from src.gui.custum_widgets.info_boxes.input_boxes import *
+
+class InputTest(unittest.TestCase):
+    def setUp(self) -> None:
+        root = tk.Tk()
+        self.__entry = EntryInput(root)
+        self.__spin = SpinInput(root, from_to=(1,1))
+        self.__dialog = DialogInput(root, default_path='NoData')
+        
+        self.__inputs: list[InputBox] = [
+            [self.__entry, 'hoge'], 
+            [self.__spin,   16],
+            [self.__dialog, 'NoData']
+        ]
+        
+    def test_is_current_value(self):
+        for input, value in self.__inputs:
+            with self.subTest(input=input, value=value):
+                input.set(value)
+                self.assertEqual(input.value(), value)
+        
+if __name__ == '__main__':
+    unittest.main()
