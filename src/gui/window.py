e4b9fd2c742b81c3526fd608402262ada5876814
Auros-NotePC <92504306+Auros-engineerVersion@users.noreply.github.com>
2023-05-16 11:58:05 +0900

    change the window

--- src/gui/window.py
+++ src/gui/window.py
@@ -2,60 +2,12 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
 
-from src.gui import custum_widgets as mytk
-from src.my_util import identity, tail_recursion, Jump
+from gui.custum_widgets import custum_widgets as mytk
+from src.my_util import identity, tail_recursion
 from src.interface.i_node import INode
 from src.setting.settings import Settings, SettingData
-import src.data.setting_data
 from src.data.nodes import Node
 from src.browser.browser_controls import BrowserControl
-
-class MainFrame(tk.Frame):
-    def __init__(self, master: tk.Misc, root: INode, width: int, height: int):
-        tk.Frame.__init__(self, master)
-        #アスペクト比を維持したまま小さいサイズにする
-        node_canvas = mytk.ScrollableFrame(self, tk.SUNKEN, width=width/5, height=height/5, padx=1, pady=1)
-        node_info = mytk.NodeInfoFrame(self)
-        root_box = mytk.NodeBox(node_canvas.scrollable_frame, node_info, root) #最初の頂点を初期化, これは動的にpackされる
-        node_info.set_box(root_box)
-        
-        node_canvas.pack(side=tk.LEFT, anchor=tk.W, fill=tk.Y)
-        node_info.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
-        
-class SettingFrame(tk.Frame):
-    def __init__(self, master: tk.Misc, data: SettingData, width: int, height: int):
-        tk.Frame.__init__(self, master)
-        
-        self.__boxes = []        
-        box_set = map(
-            lambda box: box.pack(side=tk.TOP, anchor=tk.W, fill=tk.X) if box is not None else None,
-            map(
-                lambda args: identity(mytk.box_factory(*args, width)(self))(self.__boxes.append),
-                vars(data).items()
-            )
-        )
-        list(box_set)
-        
-        set_button = tk.Button(self, text='設定', command=lambda: self.save(data.node_list, self.__boxes))
-        set_button.pack(side=tk.BOTTOM, anchor=tk.E)
-        
-        self.set(data)
-        
-    def set(self, data: SettingData):
-        data_set = map(
-            lambda d, box: box.set(d) if box is not None else None,
-            vars(data).values(), self.__boxes
-        )
-        list(data_set)
-        
-    def save(self, nodes: list, boxes: list):
-        args = [*map(lambda box: box.value(), filter(lambda box: box is not None, boxes)), nodes]
-        data = SettingData(*args)
-        
-        if messagebox.askyesno('注意', '設定を更新するためには、アプリを再起動しなければなりません。再起動しますか？'):
-            Settings.save(SettingData.SETTINGFOLDER_PATH, data)
-            self.master.master.destroy()
-            Window.RunWindow(min(data.node_list), data)
 
 class Window(tk.Frame):
     @staticmethod
