dc9d93f4a88f5e293e2a1afe15e2c00220bc3f9d
Auros-NotePC <92504306+Auros-engineerVersion@users.noreply.github.com>
2023-05-16 11:57:16 +0900

    add the variable

--- src/data/routine_data.py
+++ src/data/routine_data.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
-from typing import SupportsInt
-from datetime import date, datetime, timedelta
-from dateutil.relativedelta import relativedelta
+from typing import SupportsInt, ClassVar
+from datetime import datetime, timedelta
 
 @dataclass
 class RoutineData:
@@ -9,6 +8,14 @@ class RoutineData:
     day: SupportsInt = 0
     hour: SupportsInt = 0
     minute: SupportsInt = 0
+    
+    #週、日、時、分の最大の時間
+    week_range:  ClassVar[tuple[SupportsInt, SupportsInt]] = (0, 6)
+    day_range:   ClassVar[tuple[SupportsInt, SupportsInt]] = (0, 31)
+    hour_range:  ClassVar[tuple[SupportsInt, SupportsInt]] = (0, 23)
+    minute_range:ClassVar[tuple[SupportsInt, SupportsInt]] = (0, 59)
+    
+    time_range: ClassVar[list] = [week_range, day_range, hour_range, minute_range]
     
     def __post_init__(self):
         self.__pre_time = datetime.now().replace(microsecond=0)
