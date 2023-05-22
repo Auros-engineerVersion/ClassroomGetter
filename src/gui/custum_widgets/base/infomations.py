3c7f17bd9bf31ac638dc366aa09221e9384e332d
Auros-NotePC <92504306+Auros-engineerVersion@users.noreply.github.com>
2023-05-22 15:52:14 +0900

    change the informations

--- src/gui/custum_widgets/base/infomations.py
+++ src/gui/custum_widgets/base/infomations.py
@@ -9,6 +9,7 @@ TEXT = "text"
 STATE = 'state'
 NO_DATA = '未設定'
 RUN = '実行'
+BUTTON_PRESS = '<ButtonPress>'
 
 class NodeInfoFrame(tk.Frame):
     def __init__(self, master: tk.Misc, watching_box: NodeBox = None):
@@ -20,52 +21,49 @@ class NodeInfoFrame(tk.Frame):
         self.__time_box = Timer(self, watching_box=self.__watching_box)
         
         #ボタンが押されたら、監視中のNodeBoxからinitialize_treeを実行する
-        self.__init_button = tk.Button(self,
-            text=RUN,
-            command=lambda: self.__watching_box.initialize_node()
-        )
+        self.__init_button = tk.Button(self, text=RUN)
+        self.__init_button.bind(BUTTON_PRESS, lambda: asyncio.run(self.__watching_box.initialize_node()))
         
+#region pack
         self.__node_name_label.pack(side=tk.TOP, anchor=tk.CENTER, padx=5, pady=5)
         self.__node_url_label.pack(side=tk.TOP)
         self.__init_button.pack(side=tk.BOTTOM, fill=tk.X)
         self.__time_box.pack(side=tk.BOTTOM)
+#endregion
         
     def set_box(self, box: NodeBox):
         self.__node_name_label[TEXT] = box.text
         self.__node_url_label[TEXT] = box.url
         self.__watching_box = box
-        self.__time_box.set_box(box)
+        self.__time_box.watching_box = box
         
     def change_state(self, state: str, text: str):
         self.__init_button[STATE] = state
         self.__init_button[TEXT] = text
         self.__init_button.update()
         
+    async def run_clock_async(self, target: NodeBox):
+        self.__watching_box = target
+        self.__time_box.watching_box = target
+        await self.__time_box.update_clock(target)
+        
 class Timer(tk.Frame):
     def __init__(self, master: tk.Misc, watching_box: NodeBox):
         tk.Frame.__init__(self, master, background='yellow')
-        self.__clocK_update_interval = 1000 #ms
         self.__watching_box = watching_box
-        self.__events: list[str] = []
-
         clock_frame = tk.Frame(self)
         clock_view  = tk.Label(clock_frame, text='次の更新まで')
         self.clock_label = tk.Label(clock_frame, text=NO_DATA)
                 
         time_set_frame = tk.Frame(self)
         self.__time_setters = TimeSetters(self)
-        set_button = tk.Button(
-            time_set_frame, 
-            text='この時間に指定する',
-            command=lambda: self.__set_dateself.__time_setters.values()
-        )
+        set_button = tk.Button(time_set_frame, text='この時間に指定する')
+        set_button.bind(BUTTON_PRESS, lambda: self.__watching_box.set_time(RoutineData(*self.__time_setters.values())))
         
-        reset_button = tk.Button(
-            time_set_frame,
-            text='元に戻す',
-            command=self.__watching_box.time_reset
-        )
+        reset_button = tk.Button(time_set_frame, text='元に戻す')
+        reset_button.bind(BUTTON_PRESS, self.__watching_box.time_reset)
         
+#region pack
         clock_frame.pack()
         clock_view.pack(side=tk.TOP)
         self.clock_label.pack(side=tk.TOP)
@@ -74,26 +72,22 @@ class Timer(tk.Frame):
         self.__time_setters.pack()
         set_button.pack(side=tk.LEFT)
         reset_button.pack(side=tk.RIGHT)
+#endregion
         
-    def __cancel_all(self):
-        for id in self.__events:
-            self.after_cancel(id)
-        self.__events.clear()
+    @property
+    def watching_box(self):
+        return self.__watching_box
+        
+    async def update_clock(self, box: NodeBox, interval: int):
+        try:
+            while not box.time.should_init():
+                self.clock_label[TEXT] = box.time.remaine()
+                await asyncio.sleep(interval)
                 
-    #ループの中核
-    def __update_clock(self, box: NodeBox, interval: int):
-        remaine_time = box.time.remaine()
-        if remaine_time == timedelta():
-            self.clock_label[TEXT] = NO_DATA
-        else:
-            self.clock_label[TEXT] = remaine_time
-            
-        self.__events.append(self.after(interval, self.__update_clock, box, interval))
-
-    def set_box(self, box: NodeBox): #時計の更新を行う
-        self.__watching_box = box
-        self.__cancel_all()
-        self.__update_clock(box, self.__clocK_update_interval)
+            else:
+                await asyncio.create_task(box.initialize_node()) 
+        except asyncio.CancelledError:
+            return
 
 class TimeSetters(tk.Frame):
     def __init__(self, master: tk.Misc):
