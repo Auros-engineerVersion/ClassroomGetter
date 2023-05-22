6f5b839841469862155dae4982fded49694575cc
Auros-NotePC <92504306+Auros-engineerVersion@users.noreply.github.com>
2023-05-22 16:08:42 +0900

    delete the RunWindow()

--- src/gui/window.py
+++ src/gui/window.py
@@ -1,10 +1,10 @@
 import tkinter as tk
-from tkinter import messagebox
 from tkinter import ttk
+import asyncio
 
-from gui.custum_widgets import custum_widgets as mytk
-from src.my_util import identity, tail_recursion
-from src.interface.i_node import INode
+from src.gui.custum_widgets.info_boxes.input_boxes import *
+from gui.custum_widgets.front_frame import FrontFrame
+from src.gui.custum_widgets.setting_frame import SettingFrame
 from src.setting.settings import Settings, SettingData
 from src.data.nodes import Node
 from src.browser.browser_controls import BrowserControl
@@ -39,8 +39,8 @@ class Window(tk.Frame):
         padx_size = (5, 10)
         width = 30
         
-        email    = mytk.InputBox(master, width=width, padx=padx_size, title='email')
-        password = mytk.InputBox(master, width=width, padx=padx_size, title='password')
+        email    = EntryInput(master, width=width, padx=padx_size, title='email')
+        password = EntryInput(master, width=width, padx=padx_size, title='password')
         complete_button = tk.Button(master, text='完了', command=master.quit)
 
         # ウィジェットの配置
@@ -57,8 +57,7 @@ class Window(tk.Frame):
         return (email_info, password_info)
             
     @staticmethod
-    @tail_recursion
-    def RunWindow(root_node, cfg, master: tk.Misc = None, width: int = 600, height: int = 300):
+    async def RunWindow(root_node, cfg, master: tk.Misc = None, width: int = 600, height: int = 300):
         if master is None:
             master = tk.Tk()
 
@@ -67,13 +66,11 @@ class Window(tk.Frame):
         master.resizable(0, 0) #windowのサイズ変更を許可しない
 
         note = ttk.Notebook(master, width=width, height=height)
-        main_frame = MainFrame(note, root_node, width, height)
+        main_frame = FrontFrame(note, root_node, width, height)
         setting_frame = SettingFrame(note, cfg, width, height)
 
         note.add(main_frame, text='メイン')
         note.add(setting_frame, text='設定')
         note.pack(expand=True)
-
-        master.mainloop()
         
         return cfg
