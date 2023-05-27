24a98845f00d750ed1409e132b968d6756c83a07
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-05-27 23:16:41 +0900

    change the test

--- test/data_tests/search_parameter_test.py
+++ test/data_tests/search_parameter_test.py
@@ -9,42 +9,19 @@ from src.my_util import do_nothing
 from src.interface.i_node import INode
 from src.data.serach_parameter_container import *
 
-def add_str(x, y):
+def add_str(x, y, z):
     def __add(func):
-        return [x + y]
+        return [x + y + z]
     return __add
 
 class ParameterTest(unittest.TestCase):    
     def test_next_value(self):
+        bc = 'BC'
         param = SearchParameter(
             xpath='XPATH',
             regex='REGEX',
             attribute_func=do_nothing
         )
 
-        result = param.next_values(add_str)(do_nothing)
-        self.assertEqual([param.xpath + param.regex], list(result))
-        
-class PatternTest(unittest.TestCase):
-    def setUp(self):
-        node_mock = MagicMock()
-        node_mock.key.side_effect = [0,1,2,3,4]
-        node_mock.BrowserControl.elements = add_str
-        self.__node_mock = node_mock
-        
-        self.__pattern = SearchParameterPattern(
-            'test',
-            SearchParameter(
-                'hoge',
-                'huga',
-                do_nothing
-            ),
-            SearchParameter(
-                'HOGE',
-                'HUGA',
-                do_nothing
-            )
-        )
-    
-    def test_elements(self):       
-        self.assertEqual(self.__pattern.elements(MagicMock(), self.__node_mock), [('hogehuga', 'HOGEHUGA')])
+        result = param.next_values(bc)(add_str)(do_nothing)
+        self.assertEqual([bc + param.xpath + param.regex], list(result))
