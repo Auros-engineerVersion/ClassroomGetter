4538bc17b2673c086d8a6d1388292e82f41c8f20
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-05-27 23:44:36 +0900

    add interface

---
+++ src/interface/i_browser_controls_data.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+from abc import ABCMeta, abstractmethod, abstractproperty
+from typing import Coroutine, Callable
+
+class IBrowserControlData(metaclass=ABCMeta):
+    raise NotImplementedError
