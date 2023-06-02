4604bd0fb28802e3170e5c0ca12ecd26798df1b0
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-06-02 16:58:45 +0900

    add literals

--- src/gui/literals.py
+++ src/gui/literals.py
@@ -1,8 +1,24 @@
-ROOT_TITLE = 'ClassroomHack'
+#イベント関係
 WM_DELETE_WINDOW = 'WM_DELETE_WINDOW'
+BUTTON_PRESS = '<ButtonPress>'
 TEXT = "text"
 STATE = 'state'
+
+#単語
+ROOT_TITLE = 'ClassroomHack'
+TITLE = 'title'
+MAIN = 'メイン'
+RESET = '元に戻す'
 NO_DATA = '未設定'
+SETTING = '設定'
 RUN = '実行'
-BUTTON_PRESS = '<ButtonPress>'
 BROWS = '参照'
+COMPLETE = '完了'
+WARNING = '警告'
+
+#文章
+STOP_OR_CONTINUE = '入力の途中で閉じようとしています。\n閉じますか?'
+SETTING_RESET_MESSAGE = '設定を適用するには再起動が必要です。'
+DESCRIPTIONS = [
+    
+]
