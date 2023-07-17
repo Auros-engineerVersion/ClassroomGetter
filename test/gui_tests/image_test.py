d2557ca61a8e6768e7b276ac839737c9dbc6fdeb
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-07-18 01:52:28 +0900

    change the test can instance class

--- test/gui_tests/image_test.py
+++ test/gui_tests/image_test.py
@@ -7,18 +7,43 @@ import tkinter as tk
 import caputer
 
 from my_util import *
+from src.data.setting_data import SettingData
+from src.data.nodes import Node
 
 IMAGE_PATH = Path('test/gui_tests/images')
+WHITE_LIST = ['SettingFrame', 'FrontFrame']
 
 class ImageTest(unittest.TestCase):
+    @staticmethod
+    def __create_placeholder(annotation):
+        if annotation is tk.Misc:
+            return tk.Tk()
+        elif annotation is SettingData:
+            return SettingData()
+        elif annotation is Node:
+            return Node('key', 'url', 1)
+        elif annotation is str:
+            return 'test'
+        elif annotation is int:
+            return 1
+        else:
+            if len(annotation.__subclasses__()) > 0:
+                for cls in annotation.__subclasses__():
+                    return ImageTest.__create_placeholder(cls)
+            else:
+                return annotation(*map(lambda x: ImageTest.__create_placeholder(x), annotation.__init__.__annotations__.values()))
+        
     def setUp(self) -> None:
         if not IMAGE_PATH.exists():
             IMAGE_PATH.mkdir()
         
     def test_caputuer(self):
-        for cls in all_class_in_dir(Path('src/gui')):
-            if 'src' in str(cls):
-                caputer.caputuer(cls(), f'test/gui_tests/images/{cls}.png', overwrite=True)
+        for cls in filter(lambda cls: cls.__name__ in WHITE_LIST, all_class_in_dir(Path('src/gui'))):
+            caputer.caputuer(
+                cls(*map(lambda x: self.__create_placeholder(x), cls.__init__.__annotations__.values())),
+                f'test/gui_tests/images/{cls}.png', 
+                overwrite=True
+            )
                 
 if __name__ == '__main__':
     unittest.main()
