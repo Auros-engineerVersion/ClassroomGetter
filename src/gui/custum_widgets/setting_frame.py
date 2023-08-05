ff14887cd5baf7d87d73165ffebf4d432e312a94
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-08-05 17:44:43 +0900

    add SettingGroup

--- src/gui/custum_widgets/setting_frame.py
+++ src/gui/custum_widgets/setting_frame.py
@@ -8,13 +8,13 @@ from src.gui.custum_widgets.info_boxes.input_boxes import box_factory, InputBox
 
 class DescBox(tk.Frame):
     def __init__(self, master: tk.Misc, box: InputBox = ..., text: str = ..., font_size: int = 8):
-        tk.Frame.__init__(self, master, relief=tk.GROOVE, background=master[BACKGROUND], padx=5, pady=5)
+        tk.Frame.__init__(self, master, relief=tk.GROOVE, bd=1, background=master[BACKGROUND], padx=1, pady=1)
         box.pack(side=tk.TOP, anchor=tk.W)
         self.update_idletasks()
 
         text_box = tk.Text(self, 
-            width=box.winfo_reqwidth(),
-            height=1,
+            width=font_size * 4,
+            height=2,
             relief=tk.FLAT,
             background=master[BACKGROUND],
             font=(font.nametofont(TK_DEFAULT_FONT), font_size)
@@ -22,27 +22,45 @@ class DescBox(tk.Frame):
         
         text_box.insert(tk.END, text)
         text_box.configure(state='disabled')
-        text_box.pack(side=tk.LEFT, anchor=tk.W)
+        text_box.pack(side=tk.LEFT, anchor=tk.W, fill=tk.X, expand=True)
+        
+class SettingGroup(tk.Frame):
+    def __init__(self, master: tk.Misc, values, desc_dic):
+        tk.Frame.__init__(self, master, relief=tk.GROOVE, bd=1, background=master[BACKGROUND], padx=5, pady=5)
+        
+        self.__boxes = []
+        for value in values: #変数を取得、それを元にInputBoxを作成、さらにそれに基づくDescBoxを作成
+            input_box = box_factory(*value)(self)
+            DescBox(self, input_box, desc_dic[value[0]], font_size=8).pack(side=tk.TOP, anchor=tk.W, padx=5, pady=5)
+            self.__boxes.append(input_box)
+            
+    @property
+    def boxes(self) -> list[InputBox]:
+        descs = []
+        #子のDescBoxを取得
+        for box in self.winfo_children():
+            if type(box) is DescBox:
+                descs.append(box)
+                
+        result = []
+        #DescBoxの子のInputBoxを取得
+        for desc in descs:
+            for box in desc.winfo_children():
+                if type(box) is InputBox:
+                    result.append(box)
+        
+        return result
 
 class SettingFrame(tk.Frame):
     def __init__(self, master: tk.Misc, data: SettingData):
         tk.Frame.__init__(self, master)
         
-        self.__boxes = []        
-        box_create = map(
-            lambda box, text: DescBox(self, box, text, font_size=8) if box is not None else None,
-            map(
-                lambda args: identity(box_factory(*args)(self))(self.__boxes.append),
-                vars(data).items()
-            ),
-            SettingData.DESCRIPTIONS.values()
-        )
-
-        box_set = map(
-            lambda box: box.pack(side=tk.TOP, anchor=tk.W) if box is not None else None,
-            box_create
-        )
-        list(box_set)
+        self.__boxes = []
+        count = 0
+        for v in data.normal_data(), data.advanced_data():
+            group = SettingGroup(self, values=v, desc_dic=SettingData.DESCRIPTIONS)
+            group.pack(side=tk.LEFT, anchor=tk.NW, padx=(1, 25), pady=4)
+            self.__boxes.extend(group.boxes)
         
         setting_pop_label = tk.Label(self)
         set_button = tk.Button(self, text=SETTING, command=lambda: self.__save_and_reset_message_show(data.nodes, self.__boxes, setting_pop_label))
