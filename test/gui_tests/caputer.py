9764db6b72e0f7e5446b56820fde71e9cc06c65e
Auros-NotePC <92504306+Auros-engineerVersion@users.noreply.github.com>
2023-09-01 16:42:41 +0900

    image test change

--- test/gui_tests/caputer.py
+++ test/gui_tests/caputer.py
@@ -2,9 +2,9 @@ import tkcap
 from tkinter import Widget
 from pathlib import Path
 import PIL.Image
+from time import sleep
 
 def caputuer(target: Widget, path: Path, overwrite: bool = False) -> Path:
-    from time import sleep
     target.update_idletasks()
     sleep(0.2) #widgetが読み込まれて表示されるまで待機
     tkcap.CAP(target.winfo_toplevel()).capture(str(path), overwrite)
--- test/gui_tests/image_test.py
+++ test/gui_tests/image_test.py
@@ -5,8 +5,8 @@ import unittest
 from pathlib import Path
 import tkinter as tk
 
-from .caputer import caputuer
-from src.my_util import all_class_in_dir
+from .caputer import *
+from src.my_util import *
 from src.data import SettingData, Node
 
 current_path = lambda folder: Path(__file__).parent.joinpath(folder).resolve()
@@ -14,35 +14,45 @@ IMAGE_PATH = current_path('image')
 SAVE_PATH = current_path('save')
 WHITE_LIST = ['SettingFrame', 'FrontFrame']
 
-class ImageTest(unittest.TestCase):
-    @staticmethod
-    def __create_placeholder(annotation):
-        if annotation is tk.Misc:
-            return tk.Tk(screenName='test', baseName='testbase', className='testclass', useTk=1)
-        elif annotation is SettingData:
-            return SettingData(save_folder_path=SAVE_PATH.absolute())
-        elif annotation is Node:
-            return Node('key', 'url', 1)
-        elif annotation is str:
-            return 'test'
-        elif annotation is int:
-            return 1
+def create_placeholder(annotation):
+    if annotation is tk.Misc:
+        return tk.Tk(screenName='test', baseName='testbase', className='testclass', useTk=1)
+    elif annotation is SettingData:
+        return SettingData(save_folder_path=CommentableObj(SAVE_PATH))
+    elif annotation is Node:
+        return Node('key', 'url', 1)
+    elif annotation is str:
+        return 'test'
+    elif annotation is int:
+        return 1
+    else:
+        if len(annotation.__subclasses__()) > 0:
+            for cls in annotation.__subclasses__():
+                return create_placeholder(cls)
         else:
-            if len(annotation.__subclasses__()) > 0:
-                for cls in annotation.__subclasses__():
-                    return ImageTest.__create_placeholder(cls)
-            else:
-                return annotation(*map(lambda x: ImageTest.__create_placeholder(x), annotation.__init__.__annotations__.values()))
-        
+            return annotation(*map(lambda x: create_placeholder(x), annotation.__init__.__annotations__.values()))
+                            
+def instance_of(cls):
+    args = [create_placeholder(annotation) for annotation in cls.__init__.__annotations__.values()]
+    return cls(*args)
+
+class ImageTest(unittest.TestCase):    
     def setUp(self) -> None:
         if not IMAGE_PATH.exists():
             IMAGE_PATH.mkdir()
         
     def test_caputuer(self):
-        for cls in filter(lambda cls: cls.__name__ in WHITE_LIST, all_class_in_dir(Path('src/gui'))):
-            ins = cls(*map(lambda x: self.__create_placeholder(x), cls.__init__.__annotations__.values()))
-            ins.pack()
-            caputuer(ins, IMAGE_PATH.joinpath(f'{cls.__name__}.png'), overwrite=True)
-                            
+        from src.gui.custum_widgets import FrontFrame, SettingFrame
+        caputuer_list = [FrontFrame, SettingFrame]
+        for cls in caputuer_list:
+            with self.subTest(cls.__name__):
+                try:
+                    ins = instance_of(cls)
+                    ins.pack()
+                    caputuer(ins, IMAGE_PATH.joinpath(f'{cls.__name__}.png'), overwrite=True)
+                    self.assertTrue(True)
+                except Exception as e:
+                    self.fail(e)
+
 if __name__ == '__main__':
     unittest.main()
