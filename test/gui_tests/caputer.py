3bffdb1633a1164dad6200c63f6fad5f84f782fd
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-09-14 22:47:59 +0900

    extend wait time

--- test/gui_tests/caputer.py
+++ test/gui_tests/caputer.py
@@ -8,7 +8,7 @@ import tkcap
 
 def caputuer(target: Widget, path: Path, overwrite: bool = False) -> Path:
     target.update_idletasks()
-    sleep(0.2) #widgetが読み込まれて表示されるまで待機
+    sleep(0.3) #widgetが読み込まれて表示されるまで待機
     tkcap.CAP(target.winfo_toplevel()).capture(str(path), overwrite)
     
     return path
