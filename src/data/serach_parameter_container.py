ca9b315c6ec4eb317b08da00ad68f4760c6bc464
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-09-20 19:58:35 +0900

    refactoring this file

--- src/data/serach_parameter_container.py
+++ src/data/serach_parameter_container.py
@@ -2,146 +2,117 @@ from dataclasses import dataclass
 from typing import Callable
 
 from ..browser import *
-from ..interface import *
+from ..interface import (IBrowserControlData as IBCD, INode)
 from ..my_util import *
 
 
-@dataclass(frozen=True)
-class SearchParameter:
-    xpath: str
-    regex: str
-    attribute_func: Callable
+class SearchParameter:    
+    def __init__(self, xpath, attribute_selector, sifter):
+        self.__xpath = xpath
+        self.__attribute_selector = attribute_selector
+        self.__sifter = sifter
         
-    def next_values(self, bc_data: IBrowserControlData) -> Callable[[Callable], Callable]:
+    def get_next_values(self, bc, get) -> list[WebElement]:
         """
-        この関数はカリー化されている
         Args:
-            format_func (callable):
-                acquiring_funcの戻り値をこの関数でmapする
-                
-            acquiring_func (callable):
-                この関数は高階関数で無ければならず、最終的にIterableな値を返さなければならない
-                
-            filter_func (callable):
-                acquiring_funcの戻り値をこの関数でfilterする
+            get (callable): 値を取得する関数, 引数はxpath
+            attribute (callable): WebElementから、aタブやtext等を取得するための関数
         """
-        def __filter_func(format_func: Callable) -> Callable[[Callable], Callable]:
-            def __acquiring_func(acquiring_func: Callable) -> Callable[[Callable], map]:
-                def __map(filter_func: Callable) -> map:
-                    return map(
-                        format_func,
-                        filter_func(acquiring_func(bc_data, self.xpath, self.regex)(self.attribute_func))
-                    ) 
-                return __map
-            return __acquiring_func
-        return __filter_func
         
-@dataclass(frozen=True)
+        result = []
+        @tail_recursion
+        def loop(elems: Iterable[WebElement]):
+            elem = elems.pop(0)
+            nonlocal result
+            
+            self.__attribute_selector(elem)\
+                |pipe| self.__sifter\
+                |pipe| result.append
+
+            if len(elems) == 0:
+                return result
+            else:
+                return loop(elems)
+                    
+        return loop(get(bc, self.__xpath))
+                    
 class SearchParameterPattern:
-    pattern_name: str
-    text_param: SearchParameter
-    link_param: SearchParameter
-    
-    text_format: Callable = identity
-    link_format: Callable = identity
-    pre_proc:    Callable = identity
+    def __init__(self, key_param, url_param, elems_get_proc=search_element_all, pre_proc=identity) -> None:
+        self.__key_param = key_param
+        self.__url_param = url_param
+        self.__elems_get_proc = elems_get_proc
+        self.__pre_proc = pre_proc
     
     #func:textとlinkのペアに対して行う関数
     #text_filter, link_filter: それぞれのlistに対して行う関数
-    def name_elements_pair(self, bc_data: IBrowserControlData, node: INode):
-        def __functions(acc_f: Callable, filter_f: Callable):
-            self.pre_proc((bc_data, node))
-            if self.text_param == None or self.link_param == None:
-                return convert_to_tuple([node.key + 'の授業タブ'], [to_all_tab_link(node.url)])
-            else:
-                return convert_to_tuple(
-                        self.text_param.next_values(bc_data)(self.text_format)(acc_f)(filter_f),
-                        self.link_param.next_values(bc_data)(self.link_format)(acc_f)(filter_f)
-                    )
-        return __functions
+    def key_url_pair(self, ibc: INode, node: INode):
+        """獲得したファイルの名前とurlを返す。返り値をNodeのコンストラクタに渡すことを想定している"""        
+        #if self.key_param is None or self.url_param is None:
+        #    return [*zip([node.key + 'の授業タブ'], [to_all_tab_link(node.url)])]
+        #else:
+        
+        self.__pre_proc(node)
+        return [*zip(
+            self.__key_param.get_next_values(ibc, self.__elems_get_proc),
+            self.__url_param.get_next_values(ibc, self.__elems_get_proc))]
         
 class SearchParameterContainer:
-    browser_control_data: IBrowserControlData = None
+    browser_control_data: IBCD = None
+
+    @staticmethod
+    def next_key_url(node: INode) -> list[tuple[str, str]]:
+        """
+            渡されたnodeのtree_heightを確認し、次のnodeのkeyとurlを返す。\n
+            この関数ではwebへのアクセスが生じるため注意
+        """
+        #absを取ることで、負の値が入らないように
+        return __parameters[abs(node.tree_height)].key_url_pair(SearchParameterContainer.browser_control_data, node)
+
+def __get_text(elem: WebElement) -> str:
+    return elem.text
+
+def __get_url(elem: WebElement) -> str:
+    return elem.get_attribute('href')
+
+def __move(inode):
+    move(SearchParameterContainer.browser_control_data, inode.url)
+
+def __move_and_click(inode: INode):
+    __move(SearchParameterContainer.browser_control_data, inode)
+    click_all_sections(SearchParameterContainer.browser_control_data)
+
+def __key_parameter(xpath, sifter=identity):
+    return SearchParameter(xpath, __get_text, sifter)
+
+def __url_parameter(xpath):
+    return SearchParameter(xpath, __get_url, identity)
+
+__parameters: list[SearchParameterPattern] = [
+    #添字とtree_heightを一致させる
+    #root -> 授業一覧
+    SearchParameterPattern(
+        pre_proc=identity,
+        key_param=__key_parameter("//div[@class='YVvGBb z3vRcc-ZoZQ1']", sifter=text_filter),
+        url_param=__url_parameter("//a[@class='onkcGd ZmqAt Vx8Sxd']")
+    ),
     
-    @staticmethod
-    def __get_text(elem):
-        return elem.text
+    #授業一覧 -> 授業タブ
+    SearchParameterPattern(
+        key_param=__key_parameter("//a[contains(@guidedhelpid, 'classworkTab')]"),
+        url_param=__url_parameter("//a[contains(@guidedhelpid, 'classworkTab')]")
+    ),
     
-    @staticmethod
-    def __get_link(elem):
-        return elem.get_attribute('href')
-
-    @staticmethod
-    def __move(bc_and_node):
-        move(bc_and_node[0], bc_and_node[1].url)
-        
-    def __move_and_click(bc_and_node):
-        SearchParameterContainer.__move(bc_and_node)
-        click_all_sections(bc_and_node[0])
-
-    parameters: list[SearchParameterPattern] = [
-        #添字とtree_heightを一致させる
-        SearchParameterPattern(
-            pattern_name='Home',
-            text_param=SearchParameter(
-                "//div[@class='YVvGBb z3vRcc-ZoZQ1']",
-                '.+',
-                __get_text
-            ),
-            link_param=SearchParameter(
-                "//a[@class='onkcGd ZmqAt Vx8Sxd']",
-                "^.*/c/.{16}$",
-                __get_link
-            ),
-            text_format = text_filter,
-            pre_proc=__move
-        ),
-        
-        SearchParameterPattern(
-            pattern_name='LessonTab',
-            text_param=None,
-            link_param=None
-        ),
-        
-        SearchParameterPattern(
-            pattern_name='Sections',
-            text_param=SearchParameter(
-                "//span[@class='YVvGBb UzbjTd']",
-                ".+",
-                __get_text
-            ),
-            link_param=SearchParameter(
-                "//a[contains(@aria-label, '表示')]",
-                ".*/details$",
-                __get_link
-            ),
-            pre_proc=__move_and_click
-        ),
-        
-        SearchParameterPattern(
-            pattern_name='Details',
-            text_param=SearchParameter(
-                "//div[@class='A6dC2c QDKOcc VBEdtc-Wvd9Cc zZN2Lb-Wvd9Cc']",
-                ".+",
-                __get_text
-            ),
-            link_param=SearchParameter(
-                "//a[@class='vwNuXe JkIgWb QRiHXd MymH0d maXJsd']",
-                '.*/file/d.*',
-                __get_link
-            ),
-            pre_proc=__move
-        )
-    ]
-
-    @staticmethod
-    def elements(node: INode):
-        param = SearchParameterContainer.parameters
-        if len(param) > node.tree_height:
-            return param[node.tree_height].\
-                name_elements_pair(
-                    SearchParameterContainer.browser_control_data,
-                    node
-                )(search_element_all, elements_filter)
-        else:
-            return []
+    #授業タブ -> 授業タブのファイル一覧
+    SearchParameterPattern(
+        pre_proc=__move_and_click,
+        key_param=__key_parameter("//span[@class='YVvGBb UzbjTd']"),
+        url_param=__url_parameter("//a[contains(@aria-label, '表示')]")
+    ),
+    
+    #ファイル一覧
+    SearchParameterPattern(
+        pre_proc=__move,
+        key_param=__key_parameter("//div[@class='A6dC2c QDKOcc VBEdtc-Wvd9Cc zZN2Lb-Wvd9Cc']"),
+        url_param=__url_parameter("//a[@class='vwNuXe JkIgWb QRiHXd MymH0d maXJsd']")
+    )
+]
