36c667b0e7b9f0bf4837c507bd9f3faad8809b46
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-09-20 20:00:28 +0900

    change the setup and error handling

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
-import traceback
 import asyncio
+import traceback
+
 from pydrive2.auth import GoogleAuth
 from selenium.common.exceptions import NoSuchWindowException
 
 from src.gui.application_root import ApplicationRoot
+from src.setting import *
+
 
 def main():
     #最初の認証
     #gauth = GoogleAuth()
     #gauth.LocalWebserverAuth()h
     
-    cfg_and_bc = ApplicationRoot.setup()
-    root = ApplicationRoot(*cfg_and_bc, (400, 300))
     try:
-        root.mainloop()
-        
-    except ChildProcessError as e:
-            print('\nProcess has finished by Hand')
-
-    except NoSuchWindowException as e:
+        ApplicationRoot(try_load(), (400, 300)).mainloop()
+    except NoSuchWindowException or ChildProcessError as e:
             print('\nProcess has finished by Hand')
 
 try:
-    main()
+    if __name__ == '__main__':
+        main()
         
 except Exception as e:
     print('\033[31m')
