f77046732f37b5a47746f7f190a566deea84daa8
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-09-20 19:59:54 +0900

    change the identity

--- src/my_util.py
+++ src/my_util.py
@@ -66,7 +66,10 @@ def convert_to_tuple(list_1: Iterable, list_2: Iterable) -> list[tuple]:
     )
     
 def identity(*x):
-    return x
+    if len(x) == 1:
+        return x[0]
+    else:
+        return x
     
 def left(x = None):
     def func(f = identity):
