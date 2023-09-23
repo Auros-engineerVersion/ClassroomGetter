11cc5618836a3d510b4e5bf5c11da0a23828b2c3
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-09-23 16:28:39 +0900

    add literals

--- src/literals.py
+++ src/literals.py
@@ -1,6 +1,8 @@
 #イベント関係
 WM_DELETE_WINDOW = 'WM_DELETE_WINDOW'
 BUTTON_PRESS = '<ButtonPress>'
+NOTEBOOK_TAB_CHANGED = '<<NotebookTabChanged>>'
+CONFIGURE = '<Configure>'
 TEXT = "text"
 STATE = 'state'
 
@@ -17,11 +19,16 @@ COMPLETE = '完了'
 WARNING = '警告'
 TK_DEFAULT_FONT = 'TkDefaultFont'
 BACKGROUND = 'background'
-NOTEBOOK_TAB_CHANGED = '<<NotebookTabChanged>>'
-CONFIGURE = '<Configure>'
 GUEST_MODE = 'ゲストモード'
+DESCRIPTION = 'description'
+VALUE = 'value'
 
 #文章
 STOP_OR_CONTINUE = '入力の途中で閉じようとしています。'
 SETTING_RESET_MESSAGE = '設定を適用するには再起動が必要です。'
 PROFILE_WARNING = '入力した値が正しくありません。再入力してください。'
+USER_EMAIL_DESC = 'ユーザーのメールアドレス'
+USER_PASSWORD_DESC = 'ユーザーのパスワード'
+SAVE_FOLDER_PATH_DESC = '入手したファイルを保存する場所'
+LOADING_WAIT_TIME_DESC = 'ページの読み込みを待つ時間'
+WEB_DRIVER_OPTIONS_DESC = 'Web Driverが起動する際のオプション'
