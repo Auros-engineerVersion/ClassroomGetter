27c3499491141f5f7abc75ae08fbb474ee6b1516
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-10-04 11:03:16 +0900

    add new interface

---
+++ src/interface/i_minimalist_db.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from abc import ABCMeta, abstractmethod, abstractproperty
+from typing import Callable, Coroutine
+
+class IMinimalistID(metaclass=ABCMeta):
+    @abstractmethod
+    def value(self, db):
+        raise NotImplementedError
+    
+class IMinimalistRecode(metaclass=ABCMeta):
+    @abstractmethod
+    def __lt__(self, other):
+        raise NotImplementedError
+    
+    @abstractmethod
+    def __eq__(self, __value: IMinimalistRecode) -> bool:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def __hash__(self) -> int:
+        raise NotImplementedError
+    
+class IMinimalistDB(metaclass=ABCMeta):
+    @abstractmethod
+    def __len__(self) -> int:
+        raise NotImplementedError
+        
+    @abstractmethod
+    def add(self, recode: IMinimalistRecode) -> IMinimalistID:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def get(self, id) -> IMinimalistRecode:
+        raise NotImplementedError
+        
+    @abstractmethod
+    def remove(self, id):
+        raise NotImplementedError
