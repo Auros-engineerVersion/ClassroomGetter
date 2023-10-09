2315798567fd4f09212c0fb2db6deffed33f65c6
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-10-09 11:26:14 +0900

    add abs methods in interface

--- src/interface/i_routine_data.py
+++ src/interface/i_routine_data.py
@@ -2,4 +2,26 @@ from abc import ABCMeta, abstractmethod, abstractproperty
 
 
 class IRoutineData(metaclass=ABCMeta):
-    pass
+    @abstractmethod
+    def reset(self):
+        raise NotImplementedError
+    
+    @abstractmethod
+    def interval(self):
+        raise NotImplementedError
+    
+    @abstractmethod
+    def next(self):
+        raise NotImplementedError
+    
+    @abstractmethod
+    def remaine(self):
+        raise NotImplementedError
+    
+    @abstractmethod
+    def is_current(self):
+        raise NotImplementedError
+    
+    @abstractmethod
+    def is_valid(self):
+        raise NotImplementedError
