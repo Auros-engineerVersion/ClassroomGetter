e9885ec0e34966333785ad6c774e4bf94200b3a2
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-10-10 17:33:01 +0900

    fix a root_stop

--- src/handler/logics.py
+++ src/handler/logics.py
@@ -4,17 +4,18 @@ from ..my_util import *
 from ..data import *
 from ..my_io import *
 
+
+@higher_order
 def root_stop(root, session):
     close_thread = threading.Thread(target=session.close)\
         |pipe|  (lambda t: t.start())
-    
-    for thread in [t for t in threading.enumerate()
-                   if t not in (threading.main_thread(), close_thread)]:
+
+    for thread in [t for t in threading.enumerate() if t not in (threading.main_thread(), close_thread)]:
         thread.join()
-        
-    root.destroy()
-    close_thread.join()
-    
+
+        root.destroy()
+        close_thread.join()
+
 def e_runner_add_with_thread(event_runner, func):
     thread = threading.Thread(target=func)
     event_runner.add(thread.start)
