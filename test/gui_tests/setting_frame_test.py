ea1751e142b51331886a08940edd7d71221ace38
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-10-13 21:33:26 +0900

    fix a test

--- test/gui_tests/setting_frame_test.py
+++ test/gui_tests/setting_frame_test.py
@@ -12,10 +12,8 @@ from src.gui.custum_widgets import *
 class SettingGroupTest(unittest.TestCase):
     def test_boxes(self):
         values = {'a': {'value': '1', 'description': 'a'}, 'b': {'value': '2', 'description': 'b'}}
-        sg = SettingGroup(tk.Tk(), values)
+        sg = SettingGroup(tk.Tk(), 'hoge', values)
         
-        self.assertEqual(len(list(sg.boxes)), 2)
-        for box, excepted in zip(sg.boxes, values.values()):
-            with self.subTest(msg=f'{box}: {box.get()}', box=box):
-                self.assertIsInstance(box, InputBox)
-                self.assertEqual(box.get(), excepted['value'])
+        self.assertEqual(len(sg.values), 2)
+        self.assertEqual(sg.values[0], values['a']['value'])
+        self.assertEqual(sg.values[1], values['b']['value'])
