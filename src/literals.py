a9c1473267dac66ca3e5e4aea475850b55db23d4
Auros-engineerVersion <kurosu.0122.playstation@gmail.com>
2023-10-13 17:54:13 +0900

    tweak literals

--- src/literals.py
+++ src/literals.py
@@ -1,4 +1,4 @@
-#イベント関係
+#region イベント
 WM_DELETE_WINDOW = 'WM_DELETE_WINDOW'
 BUTTON_PRESS = '<ButtonPress>'
 NOTEBOOK_TAB_CHANGED = '<<NotebookTabChanged>>'
@@ -7,8 +7,9 @@ TEXT = "text"
 STATE = 'state'
 TK_DEFAULT_FONT = 'TkDefaultFont'
 BACKGROUND = 'background'
+#endregion
 
-#単語
+#region 単語
 MAIN = 'メイン'
 RESET = '元に戻す'
 NO_DATA = '未設定'
@@ -22,8 +23,9 @@ LOADING = '更新中'
 FINISHING = '終了中'
 NORMAL_DATA = '設定'
 ADVANCED_DATA = '高度な設定'
+#endregion
 
-#UI
+#region UI
 ROOT_TITLE = 'ClassroomHack'
 TITLE = 'title'
 GUEST_MODE = 'ゲストモード'
@@ -35,8 +37,9 @@ SAVE_FOLDER_PATH = '保存先のフォルダ'
 LOADING_WAIT_TIME = 'ページの読み込みを待つ時間'
 WEB_DRIVER_OPTIONS = 'Web Driverのオプション'
 SEARCH_DEPTH = '探索の深度'
+#endregion
 
-#文章
+#region 文章
 STOP_OR_CONTINUE = '入力の途中で閉じようとしています。'
 SETTING_RESET_MESSAGE = '設定を適用するには再起動が必要です。'
 PROFILE_WARNING = '入力した値が正しくありません。再入力してください。'
@@ -44,17 +47,32 @@ WEB_DRIVER_QUIT = 'Web Driverを終了しています。'
 
 INCLUDE_THIS_IN_PATH = '保存の際、自身をパスに含める'
 
-USER_EMAIL_DESC = 'googleアカウントのメールアドレスを入力してください。このアカウントはclassroomへのログイン、ファイルのダウンロードの際に必要となります。'
-USER_PASSWORD_DESC = 'googleアカウントのパスワードを入力してください。このアカウントはclassroomへのログイン、ファイルのダウンロードの際に必要となります。'
+USER_EMAIL_DESC = '''
+googleアカウントのメールアドレスを入力してください。
+このアカウントはclassroomへのログイン、ファイルのダウンロードの際に必要となります。'''
+
+USER_PASSWORD_DESC = '''
+googleアカウントのパスワードを入力してください。
+このアカウントはclassroomへのログイン、ファイルのダウンロードの際に必要となります。'''
+
 SAVE_FOLDER_PATH_DESC = '入手したファイルを保存する場所です。'
-LOADING_WAIT_TIME_DESC = 'ページの読み込みを待つ時間です。これがあまりにも短い場合、正常に動作しない可能性があります。ご自身のインターネット環境に合わせて設定してください。'
-WEB_DRIVER_OPTIONS_DESC = 'Web Driverが起動する際のオプションです。詳細はhttps://peter.sh/experiments/chromium-command-line-switches/を参照してください。'
+LOADING_WAIT_TIME_DESC = '''
+ページの読み込みを待つ時間です。
+これがあまりにも短い場合、正常に動作しない可能性があります。
+ご自身のインターネット環境に合わせて設定してください。'''
+
+WEB_DRIVER_OPTIONS_DESC = '''
+Web Driverが起動する際のオプションです。
+ここで指定するオプションによっては、ファイルのダウンロードが行われない可能性があります。
+詳細はhttps://peter.sh/experiments/chromium-command-line-switches/を参照してください。
+※--headlessは古いオプションです。代わりに--headless=newを使用してください。'''
+
 SEARCH_DEPTH_DESC = '''
 探索を行う際の深度です。深度により、以下のように探索する範囲が変わります。
 深度0の場合: なんちゃら学（ここで終了）
 深度1の場合: なんちゃら学 -> 第1回（ここで終了） 
-深度2の場合: なんちゃら学 -> 第1回 -> Hoge.pdf（ここで終了）
-'''
+深度2の場合: なんちゃら学 -> 第1回 -> Hoge.pdf（ここで終了）'''
+#endregion
 
 #json dictionary keys
 VALUE = 'value'
